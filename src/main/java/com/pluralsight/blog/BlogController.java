2d1ebb546df52b3de42a8c2991b6bdfdab51811f
Daria Paluch <external.daria.paluch@kuehne-nagel.com>
2022-01-04 19:38:11 +0100

    Completed module 3

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap model){
-        model.put("title","Blog Post 1");
+        List<Post> posts = postRepository.getAllPosts();
+        model.put("posts", posts);
         return "home";
     }
 
